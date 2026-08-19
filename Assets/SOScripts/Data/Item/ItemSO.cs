0a700f61ace142596d6f0fe9b81b1295a4c2c4f0
LJH00G <lhj00g@gmail.com>
2026-08-19 10:44:31 +0800

    added description to item and added item folders

--- Assets/SOScripts/Data/Item/ItemSO.cs
+++ Assets/SOScripts/Data/Item/ItemSO.cs
@@ -9,7 +9,8 @@ namespace Game.SO.Data.Item
         public string Name { get; private set; }
         [field: SerializeField]
         public Texture2D Texture { get; private set; }
-
+        [field: SerializeField]
+        public string Description { get; private set; }
 
     }
 }
