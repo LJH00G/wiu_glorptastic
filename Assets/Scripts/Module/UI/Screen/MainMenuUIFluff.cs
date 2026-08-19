50b9724912288b16f66d9f6ec677382cff7c61bd
Pieloss <jagenlim1234@gmail.com>
2026-08-19 09:21:14 +0800

    Main Menu UI
    Added main menu UI and handler scripts for some basic Main Menu Functions and fluff effects

---
+++ Assets/Scripts/Module/UI/Screen/MainMenuUIFluff.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUIFluff : MonoBehaviour
+{
+    [SerializeField] private GameObject[] buttons;
+    [SerializeField] private float rotationStep = 45f;
+    [SerializeField] private float rotationSpeed = 5f;
+
+    private int currentIndex = 0;
+    private float targetAngle;
+
+    void Start()
+    {
+        ShowOnly(currentIndex);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) Cycle(-1);
+        if (Input.GetKeyDown(KeyCode.E)) Cycle(1);
+        if (Input.GetKeyDown(KeyCode.Space)) TriggerCurrent();
+
+        float z = transform.eulerAngles.z;
+        transform.rotation = Quaternion.Euler(0f, 0f,
+            Mathf.LerpAngle(z, targetAngle, Time.deltaTime * rotationSpeed));
+    }
+
+    void Cycle(int direction)
+    {
+        currentIndex = (currentIndex + direction + buttons.Length) % buttons.Length;
+        targetAngle += direction * rotationStep;
+        ShowOnly(currentIndex);
+    }
+
+    void TriggerCurrent()
+    {
+        var button = buttons[currentIndex].GetComponent<Button>();
+        if (button != null && button.interactable)
+            button.onClick.Invoke();
+    }
+
+    void ShowOnly(int index)
+    {
+        for (int i = 0; i < buttons.Length; i++)
+            buttons[i].SetActive(i == index);
+    }
+}
