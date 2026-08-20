1197ab6683d56ed89b27275b29ea734106149c3f
Pieloss <jagenlim1234@gmail.com>
2026-08-20 15:36:46 +0800

    Updated the Inventory UI
    added the ability for people to equip relevant inventory items and they will be placed in the relevant accessory and weapon UI slots

--- Assets/SOScripts/Data/Item/Inventory/InventoryItemUI.cs
+++ Assets/SOScripts/Data/Item/Inventory/InventoryItemUI.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Game.SO.Data.Item;
-
+using Game.SO.Data.Item.Sellable.Battle;
+///add a new system that allows inventory to be reused during the shop section when the player is prompted whether they want to sell anything, inventory should pop back open, disable the underlying shop layer and let player
+///choose what items in their inventory they want to sell, then it uses the inventory description space to display the shop trading stuff like the cost you gain from trading and the amount players want to trade for it etc.
 namespace Game.Inventory
 {
     public class InventoryItemUI : MonoBehaviour
@@ -14,9 +17,20 @@ namespace Game.Inventory
         [SerializeField] TMP_Text descriptionText;
         [SerializeField] TMP_Text quantityText;
 
+        [SerializeField] Button equipButton;
+
         static readonly Dictionary<Texture2D, Sprite> spriteCache = new();
 
         public ItemSO BoundItem { get; private set; }
+
+        public event Action<ItemSO> OnEquipRequested;
+        void Awake()
+        {
+            if (equipButton)
+            {
+                equipButton.onClick.AddListener(() => OnEquipRequested?.Invoke(BoundItem));
+            }
+        }
 
         public void SetData(ItemSO item, uint quantity)
         {
@@ -37,6 +51,10 @@ namespace Game.Inventory
             if (quantityText)
             {
                 quantityText.text = quantity > 1 ? $"x{quantity}" : string.Empty;
+            }
+            if (equipButton)
+            {
+                equipButton.gameObject.SetActive(item is BattleItemSO);
             }
         }
 
