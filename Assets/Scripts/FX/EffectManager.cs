0bedded9df02bad2b68633a31618d832f357900c
Zenguin <91723603+AV307@users.noreply.github.com>
2026-08-27 16:25:22 +0800

    commit first
    wefewfwef

--- Assets/Scripts/FX/EffectManager.cs
+++ Assets/Scripts/FX/EffectManager.cs
@@ -7,8 +7,13 @@ namespace FX
     {
         public static void RecieveSignal(Effect effect)
         {
+<<<<<<< Updated upstream
             StaticGlobalVariable.GenerateParticleEventChannel.Raise(effect.particle);
             StaticGlobalVariable.PlaySFXEventChannel.Raise(effect.audio);
+=======
+            particleChannel?.Raise(effect.particle);
+            playSFXChannel?.Raise(effect.audio);
+>>>>>>> Stashed changes
         }
 
     }
---
+++ Assets/Scripts/Zhi En/Combat/Scripts/Core/AudioReferences.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using FX;
+
+public class AudioReferences : MonoBehaviour
+{
+    public Effect selectSFX;
+    public Effect attackSFX;
+    public Effect defendSFX;
+    public Effect hitSFX;
+    public Effect spellSFX;
+    public Effect killedSFX;
+    public Effect eatSFX;
+}
