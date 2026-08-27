14160cb53da9ad7e21c7b25ecbab3d02be146ecb
Zenguin <91723603+AV307@users.noreply.github.com>
2026-08-27 19:47:11 +0800

    hopefully jiaheng can get the sprites back

--- Assets/Scripts/Specific/Game/GameIniter.cs
+++ Assets/Scripts/Specific/Game/GameIniter.cs
@@ -31,6 +31,10 @@ public class GameIniter : MonoBehaviour
         GameManager.SetOverWorldState(OVERWORLD_STATE.GENERAL);
         foreach(string flagName in Flags)
             GameManager.EnsureFlag(flagName);
+
+        GameManager.CurrentUserData.PlayerBattleData.Refresh();
+        GameManager.CurrentUserData.PlayerBattleData.CurrentHP = GameManager.CurrentUserData.PlayerBattleData.MaxHP;
+        GameManager.CurrentUserData.PlayerBattleData.CurrentCurse = GameManager.CurrentUserData.PlayerBattleData.MaxCurse;
     }
 
     private void Start()
