da65497bb51493a440fd37a0b69fd6e5e09547c3
Themiei <themieigab@gmail.com>
2026-08-27 19:00:05 +0800

    forgot line

--- Assets/SOScripts/ActionFn/Derived/EventChannel/Specific/ShopPresetLoadEventChannelActionSO.cs
+++ Assets/SOScripts/ActionFn/Derived/EventChannel/Specific/ShopPresetLoadEventChannelActionSO.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 using Game.SO.EventChannel;
 using Game.SO.Data.Shop;
 
-
-
 namespace Game.SO.ActionFn
 {
-
+    [CreateAssetMenu(fileName = "ShopPresetLoadEventChannel_Act", menuName = "Scriptable Objects/ActionFn/EventChannel/ShopPresetLoadEventChannelActionSO")]
     public class ShopPresetLoadEventChannelActionSO : EventChannelActionSO<ShopOpenEventChannelSO, ShopPresetSO>
 
     {
